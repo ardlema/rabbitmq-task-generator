646ed03330785d51b96e71726b373cfbc59a3465
Alberto <ardlema@gmail.com>
2013-12-30 13:50:32 +0100

    Task generator working fine

--- src/main/scala/TaskGenerator.scala
+++ src/main/scala/TaskGenerator.scala
@@ -1,9 +1,34 @@
 package ardlema.rabbitmqtaskgenerator
 
+import com.rabbitmq.client.{MessageProperties, ConnectionFactory}
+
 object TaskGenerator {
 
   def main(args: Array[String]) {
-    println("HOLAAA!!!!")
+    val queueName = "taskQueue"
+    val factory = new ConnectionFactory()
+    factory.setHost("localhost")
+    val connection = factory.newConnection()
+    val channel = connection.createChannel()
+
+    channel.queueDeclare(queueName, true, false, false, null)
+
+    val message = getMessage(args)
+
+    channel.basicPublish( "", queueName,
+    MessageProperties.PERSISTENT_TEXT_PLAIN,
+      message.getBytes())
+    println(" [x] Sent '" + message + "'")
+
+    channel.close();
+    connection.close()
   }
+
+  def getMessage(args: Array[String]) = {
+    if (args.length < 1)
+      "Hello World!"
+    else args(0)
+  }
+
 
 }
